3bcc128d6fa3e3661326011bb70edd4bd654d746
eanikulin <ea.nikulin@yandex.ru>
2022-03-29 17:09:40 +0500

    //add base template

--- sky_framework/templator.py
+++ sky_framework/templator.py
@@ -1,5 +1,7 @@
-from jinja2 import Template
-from os.path import join
+# from jinja2 import Template
+# from os.path import join
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
@@ -9,10 +11,17 @@ def render(template_name, folder='templates', **kwargs):
     :param kwargs: параметры
     :return:
     """
-    file_path = join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='utf-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # рендерим шаблон с параметрами
+    # file_path = join(folder, template_name)
+    # # Открываем шаблон по имени
+    # with open(file_path, encoding='utf-8') as f:
+    #     # Читаем
+    #     template = Template(f.read())
+    # # рендерим шаблон с параметрами
+    # return template.render(**kwargs)
+
+    env = Environment()
+    # указываем папку для поиска шаблонов
+    env.loader = FileSystemLoader(folder)
+    # находим шаблон в окружении
+    template = env.get_template(template_name)
     return template.render(**kwargs)
